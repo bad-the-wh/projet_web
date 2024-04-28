866f6919f1fabbd6e4ec0f17a64fd9fc7f480335
Ludoreve <ludovic.revel@viacesi.fr>
2024-04-28 12:51:26 +0200

    1er commit

---
+++ sidebar.php
@@ -0,0 +1,73 @@
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <div class="dropdown">
+   	<a class="brand-link">
+      <h3 class="text-center p-0 m-0"><b>Project Time</b></h3>
+    </a>
+      
+    </div>
+    <div class="sidebar pb-4 mb-4">
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column nav-flat" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item dropdown">
+            <a href="./" class="nav-link nav-home">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="indexM.php" class="nav-link nav-hom">
+            <i class="nav-icon fas fa-solid fa-comment"></i>
+              <p>
+                Messagerie
+              </p>
+            </a>
+          </li>  
+          <li class="nav-item">
+            <a href="#" class="nav-link nav-edit_project nav-view_project">
+              <i class="nav-icon fas fa-layer-group"></i>
+              <p>
+                Projects
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./index.php?page=new_project" class="nav-link nav-new_project tree-item">
+                  <i class="fas fa-angle-right nav-icon"></i>
+                  <p>Add New</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./index.php?page=project_list" class="nav-link nav-project_list tree-item">
+                  <i class="fas fa-angle-right nav-icon"></i>
+                  <p>List</p>
+                </a>
+              </li>
+            </ul>
+          </li> 
+        </ul>
+      </nav>
+    </div>
+  </aside>
+  <script>
+  	$(document).ready(function(){
+      var page = '<?php echo isset($_GET['page']) ? $_GET['page'] : 'home' ?>';
+  		var s = '<?php echo isset($_GET['s']) ? $_GET['s'] : '' ?>';
+      if(s!='')
+        page = page+'_'+s;
+  		if($('.nav-link.nav-'+page).length > 0){
+             $('.nav-link.nav-'+page).addClass('active')
+  			if($('.nav-link.nav-'+page).hasClass('tree-item') == true){
+            $('.nav-link.nav-'+page).closest('.nav-treeview').siblings('a').addClass('active')
+  				$('.nav-link.nav-'+page).closest('.nav-treeview').parent().addClass('menu-open')
+  			}
+        if($('.nav-link.nav-'+page).hasClass('nav-is-tree') == true){
+          $('.nav-link.nav-'+page).parent().addClass('menu-open')
+        }
+
+  		}
+     
+  	})
+  </script>
---
+++ topbar.php
@@ -0,0 +1,38 @@
+<!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-primary navbar-dark ">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+     
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+     <li class="nav-item dropdown">
+            <a class="nav-link"  data-toggle="dropdown" aria-expanded="true" href="javascript:void(0)">
+              <span>
+                <div class="d-felx badge-pill">
+                  <span class="fa fa-user mr-2"></span>
+                  <span><b>Administrateur</b></span>
+                  <span class="fa fa-angle-down ml-2"></span>
+                </div>
+              </span>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="account_settings" style="left: -2.5em;">
+              <a class="dropdown-item" href="javascript:void(0)" id="manage_account"><i class="fa fa-cog"></i> Manage Account</a>
+            </div>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+  <script>
+     $('#manage_account').click(function(){
+        uni_modal('Manage Account','manage_user.php?id=<?php echo $_SESSION['login_id'] ?>')
+      })
+  </script>
